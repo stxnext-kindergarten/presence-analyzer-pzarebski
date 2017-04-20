a9edcbd1400b2c8204d12776a95949c476c0dcb2
Paweł Zarębski <pawel.zarebski@stxnext.com>
2017-04-20 16:50:52 +0200

    add timeline screen

--- src/presence_analyzer/views.py
+++ src/presence_analyzer/views.py
@@ -7,7 +7,10 @@ import calendar
 from flask import redirect, abort
 
 from presence_analyzer.main import app
-from presence_analyzer.utils import jsonify, get_data, mean, group_by_weekday
+from presence_analyzer.utils import (
+    jsonify, get_data, mean,
+    group_by_weekday, group_by_weekday_start_end,
+)
 
 import logging
 log = logging.getLogger(__name__)  # pylint: disable=invalid-name
@@ -72,4 +75,29 @@ def presence_weekday_view(user_id):
     ]
 
     result.insert(0, ('Weekday', 'Presence (s)'))
+    return result
+
+
+@app.route('/api/v1/presence_start_end/<int:user_id>', methods=['GET'])
+@jsonify
+def presence_start_end_view(user_id):
+    """
+    Returns mean start time and mean end time for given user
+    grouped by weekday.
+    """
+    data = get_data()
+    if user_id not in data:
+        log.debug('User %s not found!', user_id)
+        abort(404)
+
+    weekdays = group_by_weekday_start_end(data[user_id])
+    result = [
+        (
+            calendar.day_abbr[weekday],
+            mean(time.get('start', [])),
+            mean(time.get('end', [])),
+        )
+        for weekday, time in enumerate(weekdays)
+    ]
+
     return result
